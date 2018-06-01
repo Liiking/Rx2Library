7902a49827b140795e1ada2139a8a99e4a44c24c
qwy <1695173542@qq.com>
2018-06-01 15:52:43 +0800

    图片大图预览

--- app/src/main/java/com/http/httpdemo/util/MyImageLoader.java
+++ app/src/main/java/com/http/httpdemo/util/MyImageLoader.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.lzy.imagepicker.loader.ImageLoader;
 
 import java.io.File;
@@ -16,7 +17,7 @@ public class MyImageLoader implements ImageLoader {
 
     @Override
     public void displayImage(Activity activity, String path, ImageView imageView, int width, int height) {
-        Glide.with(activity)//
+        Glide.with(activity)
                 .load(Uri.fromFile(new File(path)))
 //                .placeholder(R.mipmap.default_image)
 //                .error(R.mipmap.default_image)
@@ -25,7 +26,10 @@ public class MyImageLoader implements ImageLoader {
 
     @Override
     public void displayImagePreview(Activity activity, String s, ImageView imageView, int i, int i1) {
-
+        Glide.with(activity)                                // 配置上下文
+                .load(Uri.fromFile(new File(s)))            // 设置图片路径(fix #8,文件名包含%符号 无法识别和显示)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)   // 缓存全尺寸
+                .into(imageView);
     }
 
     @Override
